68384ee0b21fbc24586222534bf6da3c005c794f
Хворых Виталий <vithvo@bk.ru>
2022-08-02 11:02:17 +0700

    create useState with class

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import "./App.css";
 import { Fcomponent } from "./Components/Fcomponent";
 
 function App() {
-  apple();
-  setInterval(apple, 1000);
   return <Fcomponent />;
 }
 export default App;
--- src/Components/Fcomponent.js
+++ src/Components/Fcomponent.js
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { Component } from "react";
 
-export const Fcomponent = () => {
-  const apple = () => {
-    const time = new Date().toLocaleTimeString();
+export class Fcomponent extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      count: 0,
+    };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  increment() {
+    this.setState((state) => ({
+      count: state.count + 1,
+    }));
+  }
+  decrement() {
+    this.setState((state) => ({
+      count: state.count - 1,
+    }));
+  }
+  reset() {
+    this.setState({ count: 0 });
+  }
+
+  render() {
     return (
       <div>
-        <h1>Functional component</h1>
-        <h2>{time}</h2>
+        <button className="button" onClick={this.increment}>
+          Прибавить
+        </button>
+        <button className="button" onClick={this.decrement}>
+          Отнять
+        </button>
+        <button className="button" onClick={this.reset}>
+          Обнулить
+        </button>
+
+        <h1>Cuttent: {this.state.count}</h1>
       </div>
     );
-  };
-
-};
+  }
+}
